9f03e80a2fd56bed9ea68c7d9f3fb4ba75a44e13
svetlana-e-petrova <svetlana.evg.petrova@gmail.com>
2017-02-19 13:38:18 +0300

    add repository package, create abstract class TaskRepository and it's SQLite implementation

---
+++ app/src/main/java/org/jasey/unforgetit/repository/DataBaseHelper.java
@@ -0,0 +1,55 @@
+package org.jasey.unforgetit.repository;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import org.jasey.unforgetit.entity.Task;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+
+    private static final String CREATE_TABLE_SCRIPT =
+            "CREATE TABLE " + Task.TABLE_NAME +
+                    " (" + Task.TASK_DATE_COLUMN + " BIGINT NOT NULL , " +
+                    Task.TASK_DONE_COLUMN + " SMALLINT NOT NULL , " +
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT , " +
+                    Task.TASK_PRIORITY_COLUMN + " INTEGER NOT NULL , " +
+                    Task.TASK_TITLE_COLUMN + " VARCHAR NOT NULL" +
+                    ");";
+
+    private static DataBaseHelper helper;
+
+    synchronized static DataBaseHelper getInstance(Context context) {
+        return (helper = helper == null ? new DataBaseHelper(context) : helper);
+    }
+
+    private DataBaseHelper(Context context) {
+        super(context, TaskRepository.DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        try {
+            Log.i(this.getClass().getName(), "onCreate");
+            db.execSQL(CREATE_TABLE_SCRIPT);
+        } catch (SQLException e) {
+            Log.e(this.getClass().getName(), "Can't create database", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        try {
+            Log.i(this.getClass().getName(), "onUpgrade");
+            db.execSQL("DROP TABLE" + Task.TABLE_NAME);
+            onCreate(db);
+        } catch (SQLException e) {
+            Log.e(this.getClass().getName(), "Can't drop databases", e);
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ app/src/main/java/org/jasey/unforgetit/repository/TaskRepository.java
@@ -0,0 +1,39 @@
+package org.jasey.unforgetit.repository;
+
+import android.content.Context;
+
+import org.jasey.unforgetit.entity.Task;
+
+import java.util.List;
+
+public abstract class TaskRepository {
+    static String DATABASE_NAME = "unforget_it_db";
+
+    static String ADD = "addNew";
+    static String REMOVE = "remove";
+    static String UPDATE = "update";
+    static String GET_ALL = "getAll";
+    static String ERROR_MESSAGE = " failed";
+    static String STOPWATCH = " took %d ms.";
+
+    public static TaskRepository getInstance(Type type, Context context) {
+        switch (type) {
+            case DEFAULT:
+                return TaskRepositoryImpl.getInstance(context);
+            default:
+                return null;
+        }
+    }
+
+    public abstract boolean addNew(Task task);
+
+    public abstract boolean remove(Task task);
+
+    public abstract boolean update(Task task);
+
+    public abstract List<Task> getAll();
+
+    public enum Type {
+        DEFAULT
+    }
+}
