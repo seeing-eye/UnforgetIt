a982c94b83949eb8f6bc9485fd188c436d86abe5
svetlana-e-petrova <svetlana.evg.petrova@gmail.com>
2017-02-19 19:08:11 +0300

    [UI-2]: format date, color card views

--- app/src/main/java/org/jasey/unforgetit/adapter/MyRecyclerViewAdapter.java
+++ app/src/main/java/org/jasey/unforgetit/adapter/MyRecyclerViewAdapter.java
@@ -1,5 +1,6 @@
 package org.jasey.unforgetit.adapter;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,16 +10,19 @@ import android.widget.TextView;
 
 import org.jasey.unforgetit.R;
 import org.jasey.unforgetit.entity.Task;
+import org.jasey.unforgetit.utils.TaskUtil;
+
+import java.util.Date;
 
 public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder> {
     private Task[] mTaskSet;
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    static class ViewHolder extends RecyclerView.ViewHolder {
         CardView cv;
         TextView title;
         TextView date;
 
-        public ViewHolder(View v) {
+        ViewHolder(View v) {
             super(v);
             cv = (CardView) v.findViewById(R.id.card_view);
             title = (TextView) v.findViewById(R.id.title);
@@ -37,14 +41,35 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
         View v = LayoutInflater.from(parent.getContext())
                                 .inflate(R.layout.card_view_item, parent, false);
 
-        ViewHolder vh = new ViewHolder(v);
-        return vh;
+        return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.title.setText(mTaskSet[position].getTitle());
-        holder.date.setText(mTaskSet[position].getDate().toString());
+        Task t = mTaskSet[position];
+
+        holder.title.setText(t.getTitle());
+        holder.date.setText(TaskUtil.formatDate(t.getDate()));
+
+        switch (t.getPriorityLevel()) {
+            case (Task.PRIORITY_LOW):
+                holder.cv.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.getContext(), R.color.colorBlue));
+                break;
+            case (Task.PRIORITY_NORMAL):
+                holder.cv.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.getContext(), R.color.colorYellow));
+                break;
+            case (Task.PRIORITY_HIGH):
+                holder.cv.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.getContext(), R.color.colorOrange));
+                break;
+        }
+
+        if (t.getDate().compareTo(new Date()) <= 0) {
+            holder.cv.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.getContext(), R.color.colorRed));
+        }
+
+        if (t.isDone()) {
+            holder.cv.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.getContext(), R.color.colorGreen));
+        }
     }
 
     @Override
